e1dbfffba01a554f05ba0e69b4e8fa0fafc8355a
KingShark1 <manastiwari28@gmail.com>
2022-04-16 00:28:39 +0530

    update main.py

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from torchvision import transforms
 import torch
 
 from utils import display
+from utils.dataloader import read_image_name
 from utils.preprocess import applied_transforms, load_image, read_content
 
 from models.alexnet import AlexNet
 
+image_data_dir = 'data/detection/VOC2007/JPEGImages'
+bbox_data_dir = 'data/detection/VOC2007/Annotations'
+trainval_img_dir = 'data/detection/VOC2007/ImageSets/Main/trainval.txt' 
+test_img_dir = 'data/detection/VOC2007/ImageSets/Main/test.txt'
+
 def main():
-	input_tensor, name, bbox = AlexNet.load_image()
+	input_tensor = AlexNet.load_image(15)
 	input_batch = input_tensor.unsqueeze(0)
 	tensor_to_img = transforms.ToPILImage()
 	
@@ -16,40 +22,18 @@ def main():
 	display.show_image(tvision_transform)
 	
 
-	model = AlexNet.load_model()
+	# model = AlexNet.load_model()
 
-	if torch.cuda.is_available():
-		input_batch = input_batch.to('cuda')
-		model.to('cuda')
+	# if torch.cuda.is_available():
+	# 	input_batch = input_batch.to('cuda')
+	# 	model.to('cuda')
 
-	with torch.no_grad():
-		output = model(input_batch)
-	
-	print(output[0].size())
+	# with torch.no_grad():
+	# 	output = model(input_batch)
 
-	probabilities = torch.nn.functional.softmax(output[0], dim=0)
-	print(probabilities.size())
-	"""
-	image_path = ['./data/detection/VOC2007/JPEGImages/IP000000003.jpg', 'data/detection/VOC2007/JPEGImages/IP102005722.jpg']
-	image_crop = ['./data/detection/VOC2007/Annotations/IP000000003.xml', 'data/detection/VOC2007/Annotations/IP102005722.xml']
+	train_val_imageset = read_image_name(trainval_img_dir)
+	print(train_val_imageset[0])
 	
-	image = load_image(image_path[0])
-	# gray_image = color_to_gray(image)
-	# cannyImg = canny_detection(gray_image)
-	
-	name, bbox = read_content(image_crop[0])
-	
-	print(f"{name}\nbounding boxes : {bbox}")
-
-	# insects = crop(cannyImg, bbox)
-	# for i in insects:
-	# 	i = resize(i)
-	# 	display.show_image(i)
-	tensor_to_img = transforms.ToPILImage()
-	
-	tvision_transform = tensor_to_img(applied_transforms(image, bbox))
-	display.show_image(tvision_transform)
-	"""
 
 if __name__=="__main__":
 	main()
