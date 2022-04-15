740f90f6815c6c1e6a7bd528fd68d4c8bcc9bb82
KingShark1 <manastiwari28@gmail.com>
2022-04-16 00:28:10 +0530

    configure Alexnet.load_image() to show image according to Dataset class

--- models/alexnet/AlexNet.py
+++ models/alexnet/AlexNet.py
@@ -1,7 +1,13 @@
 import torch
 from PIL import Image
+from utils.dataloader import InsectDataset
 from utils.preprocess import applied_transforms
-from utils.dataloader import load_image_and_bbox_paths
+
+image_data_dir = 'data/detection/VOC2007/JPEGImages'
+bbox_data_dir = 'data/detection/VOC2007/Annotations'
+trainval_img_dir = 'data/detection/VOC2007/ImageSets/Main/trainval.txt' 
+test_img_dir = 'data/detection/VOC2007/ImageSets/Main/test.txt'
+
 def load_model():
 	model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', pretrained=True)
 	
@@ -10,14 +16,13 @@ def load_model():
 	model.eval()
 	return model
 
-def load_image():
+def load_image(idx):
 	"""
 	Loads, processes and return transformed Image in PIL Image format
 	also returns name of class and bboxes of the insect, in the original image (in that order)
 	"""
 
-	image_path, bbox_path = load_image_and_bbox_paths()
+	train_dataset = InsectDataset(trainval_img_dir, image_data_dir, bbox_data_dir)
 
-	tvision_transform, name, bbox = applied_transforms(image_path['train'][0], bbox_path['train'][0])
-	return tvision_transform, name, bbox
-
+	tvision_transform = train_dataset[idx]
+	return tvision_transform
